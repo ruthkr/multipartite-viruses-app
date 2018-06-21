12e6bdd1f1c366c17b71c5e5ec1a87b8f8c28a9c
Ruth Kristianingsih <ruth.kristianingsih30@gmail.com>
2018-06-21 10:28:44 +0200

    Rename scripts to clean data for 3D phase portraits

--- 6eq_3d_phase_portrait_divide_data.sh
+++ 6eq_3d_phase_portrait_divide_data.sh
@@ -9,7 +9,7 @@ outfile_name=$2
 
 # Looping over N
 for G in 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1; do
-	awk -v gamma=$G -f 6eq_clean_3d_phase_portrait_points.awk $directory/$infile > $directory/$outfile_name$G.csv;
-	awk -v gamma=$G -f 6eq_clean_3d_phase_portrait_vectors.awk $directory/$infile > $directory/$outfile_name$G-vectors.csv;
+	awk -v gamma=$G -f 6eq_3d_phase_portrait_clean_points.awk $directory/$infile > $directory/$outfile_name$G.csv;
+	awk -v gamma=$G -f 6eq_3d_phase_portrait_clean_vectors.awk $directory/$infile > $directory/$outfile_name$G-vectors.csv;
 done
 
